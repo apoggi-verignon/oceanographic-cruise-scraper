c76149020d7ccbbaf60fb486f340c690acc71d61
Arthur POGGI-VERIGNON <arthur.poggiverignon@free.fr>
2024-04-20 10:34:15 +0200

    Add cli

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import click
 
 from oceanic_cruise import OceanicCruise
 
-if __name__ == "__main__":
-    url = "https://campagnes.flotteoceanographique.fr/campagnes/18001350/fr/index.htm"
-    # page = urlopen(url)
-    # html = page.read().decode("utf-8")
-
-    html = open("test_page.html", "r").read()
-
+@click.command()
+@click.argument('doi')
+@click.argument('filepath', type=click.Path())
+@click.option('--append', is_flag=True, help='Append result to existing file')
+def main(doi, filepath, append):
+    """Extract oceanographic cruise information based on DOI"""
+    
+    url = f"https://campagnes.flotteoceanographique.fr/campagnes/{doi}/fr/index.htm"
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
+
     oceanic_cruise = OceanicCruise(soup)
-    oceanic_cruise.to_csv("test_output.csv", 'w')
+    mode = 'a' if append else 'w'
+    oceanic_cruise.to_csv(filepath, mode)
+
+if __name__ == "__main__":
+    main()
