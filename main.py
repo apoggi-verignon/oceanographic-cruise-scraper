c6e740a44778df775176bfdccd7f245239de6dd2
Arthur POGGI-VERIGNON <arthur.poggiverignon@free.fr>
2024-04-20 15:37:59 +0200

    Add output messages and exception handling

--- main.py
+++ main.py
@@ -10,15 +10,21 @@ from oceanic_cruise import OceanicCruise
 @click.option('--append', is_flag=True, help='Append result to existing file')
 def main(doi, filepath, append):
     """Extract oceanographic cruise information based on DOI"""
-    
-    url = f"https://campagnes.flotteoceanographique.fr/campagnes/{doi}/fr/index.htm"
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
 
-    oceanic_cruise = OceanicCruise(soup)
-    mode = 'a' if append else 'w'
-    oceanic_cruise.to_csv(filepath, mode)
+    try:
+        url = f"https://campagnes.flotteoceanographique.fr/campagnes/{doi}/fr/index.htm"
+        page = urlopen(url)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+
+        oceanic_cruise = OceanicCruise(soup)
+        mode = 'a' if append else 'w'
+        oceanic_cruise.to_csv(filepath, mode)
+
+        click.echo(f"Successfully extracted {doi} information to {filepath}")
+
+    except Exception as e:
+        raise click.ClickException(e)
 
 if __name__ == "__main__":
     main()
